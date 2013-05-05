dbefce496cd8f15ecdca20c5fdf1423672983770
gpimm <george.pimm@naturalmotion.com>
2013-05-05 19:39:45 +0100

    model work

--- GraphicsCore/GCSceneExplorer.cpp
+++ GraphicsCore/GCSceneExplorer.cpp
@@ -1,7 +1,39 @@
 #include "GCSceneExplorer.h"
+#include "QToolBar"
+#include "shift/sdatabase.h"
+#include "3D/GCScene.h"
+#include "QTreeView"
+#include "QVBoxLayout"
+#include "QDebug"
 
-GCSceneExplorer::GCSceneExplorer(GCScene *scene, QWidget *parent)
-    : _scene(scene)
+GCSceneExplorer::GCSceneExplorer(GCScene *scene)
+    : QWidget(),
+      _scene(scene),
+      _model(scene->database(), scene->parentEntity(), Shift::DatabaseModel::NoOptions),
+      _inputModel(scene->database(),
+                  scene,
+                  GCRenderable::staticTypeInformation(),
+                  GCRenderArray::staticTypeInformation(),
+                  GCRenderArray::staticTypeInformation()->childFromName("renderGroup"))
   {
+  QVBoxLayout *layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0, 0, 0, 0);
 
+  QToolBar *toolbar = new QToolBar(this);
+  layout->addWidget(toolbar);
+
+  toolbar->addAction("Create", this, SLOT(create()));
+
+  QTreeView *view = new QTreeView(this);
+  layout->addWidget(view);
+  view->setModel(&_inputModel);
+
+  QTreeView *debugView = new QTreeView(this);
+  layout->addWidget(debugView);
+  debugView->setModel(&_model);
+  }
+
+void GCSceneExplorer::create()
+  {
+  qDebug() << "Create";
   }
--- GraphicsCore/GCSceneExplorer.h
+++ GraphicsCore/GCSceneExplorer.h
@@ -1,17 +1,26 @@
 #ifndef GCSCENEEXPLORER_H
 #define GCSCENEEXPLORER_H
 
-#include "QWidget"
+#include "QtWidgets/QWidget"
+#include "GCGlobal.h"
+#include "shift/UI/sdatabasemodel.h"
 
 class GCScene;
 
-class GCSceneExplorer : public QWidget
+class GRAPHICSCORE_EXPORT GCSceneExplorer : public QWidget
   {
+  Q_OBJECT
+
 public:
-  GCSceneExplorer(GCScene *scene, QWidget *parent=0);
+  GCSceneExplorer(GCScene *scene);
+
+public slots:
+  void create();
 
 private:
   GCScene *_scene;
+  Shift::DatabaseModel _model;
+  Shift::InputModel _inputModel;
   };
 
 #endif // GCSCENEEXPLORER_H
