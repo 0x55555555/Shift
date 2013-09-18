e25bbe0dcac68724da964a35434a9cfc56e03063
Jorj Pimm <jorjpimm@gmail.com>
2013-09-18 22:24:18 +0100

    swtich for auto

--- GraphicsCore/3D/Manipulators/GCManipulator.cpp
+++ GraphicsCore/3D/Manipulators/GCManipulator.cpp
@@ -356,11 +356,11 @@ void GCAngularDragManipulator::onDrag(const MouseMoveEvent &e, Eks::Quaternion &
 
     Eks::Frame f(p.normal());
     
-    Eks::Vector3D isectA = a.sample(p.intersection(a));
-    Eks::Vector3D isectB = b.sample(p.intersection(b));
+    auto isectA = a.sample(p.intersection(a));
+    auto isectB = b.sample(p.intersection(b));
     
-    Eks::Vector3D subA = isectA - focus;
-    Eks::Vector3D subB = isectB - focus;
+    auto subA = isectA - focus;
+    auto subB = isectB - focus;
 
     Eks::Vector3D lastHitDir = subA.normalized();
     Eks::Vector3D hitDir = subB.normalized();
